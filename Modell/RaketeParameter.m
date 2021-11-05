0f0a8ef354c87ad550bd2c7b1d4a8a7e0d83b211
HeadAndToes <tim.toenz@gmail.com>
2021-11-05 16:41:55 +0100

    Simulink Seems to work set phisical parameters

--- Modell/RaketeParameter.m
+++ Modell/RaketeParameter.m
@@ -9,21 +9,21 @@ YawTraegheit = 0.0335;%gemessen 29.10
 PropGain = (ObjectMass*Gravitation)/0.8; % prop signal * Propgain = auftriebKraft .. Annahme bei 0.8 schwebend
 PropOffset = 0.08; %Abstand von Propeller zu Massepunkt
 DriftGain = 1; %  wieviel Kraft generiert eine Schieflage in rad
-FlapGain = 1; % (abweichung von 0 pos in rad) * fla
+FlapGain = 1.3178; % (abweichung von 0 pos in rad) * FlapGain = Drehmoment auf Objekt Approximiert von Messungen
 
 
 
 %% Regler Parameter
-KpX = 1;
-KiX = .5;
+KpX = 5;
+KiX = 0.5;
 KpY = 5;
-KiY = .5;
+KiY = 0.5;
 KpZ = 10;
-KiZ = 1;
+KiZ = 10;
 KpPitch = 1;
 KiPitch = 1;
 KpRoll = 1;
-KiRoll = 1.1;
+KiRoll = 1;
 KpYaw = 1;
 KiYaw = 1;
 
--- PhysikMessungen/AileronsGainCalculation.m
+++ PhysikMessungen/AileronsGainCalculation.m
@@ -1,4 +1,4 @@
-close all
+close
 clc
 
 run("getAileronsData.m")
@@ -8,3 +8,7 @@ hold on
 plot(angles1,torque1,angles2,torque2,angles3,torque3,angles4,torque4,angles5,torque5,Marker="*")
 legend('Engine:80% #1','Engine:80% #2','Engine:100% #3,','Engine:90% #4','Engine:95% #5')
 grid on
+flapgain = (0.92*360)/(2*pi*40)
+x=1:60;
+yAprox= x.*(2*pi*flapgain/360);
+plot(x,yAprox)
