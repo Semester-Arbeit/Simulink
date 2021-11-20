811a698842eba229f30380f30739cb064769a9bb
HeadAndToes <tim.toenz@gmail.com>
2021-11-20 20:16:12 +0100

    Sensor eval & onboard Regler

--- Modell/ReglerVergleich_.m
+++ Modell/ReglerVergleich_.m
@@ -4,18 +4,31 @@ clc
 %% Get Data Real Data
 run RaketeParameter.m;
 warning('OFF', 'MATLAB:table:ModifiedAndSavedVarnames')
-M = readtable("1_FullFlight.csv");
-M = removevars(M,{'cY_I'});
+M = readtable("../Data/SensorTest_18.11/FlightLog02.csv");
+%M = removevars(M,{'cY_I'});
 M.Time = M.Time./1000000; % in sekunden umwandeln
 M.Time = M.Time - M.Time(1);% setuptime bis zum Flugstart abziehen
-sampleTime = max(M.Time)/length(M.Time);% approx zeit zwischen samples
+sampleTime = max(M.Time)/length(M.Time);% approx zeit zwischen samples wird im SIMULINK benötigt
 %% change Regler Data
-RegPar.KpPitch= -0;
-RegPar.KiPitch= -2;
+RakParam = table();
+RakParam.KpX = 0;
+RakParam.KiX = 0;
+RakParam.KpY = 0;
+RakParam.KiY = 0;
+RakParam.KpZ = 0;
+RakParam.KiZ = 0;
+RakParam.KpPitch = 0;
+RakParam.KiPitch = 0;
+RakParam.KpRoll = 0;
+RakParam.KiRoll = 0;
+RakParam.KpYaw = 0;
+RakParam.KiYaw = 0;
 
 
 
 %% Simulate  Data
+
+
 
 out=sim("ReglerVergleich.slx");
 T = timeseries2timetable(out.SimRegler);
@@ -26,35 +39,40 @@ T = timeseries2timetable(out.FlightData);
 FlightData = table(T.Time  ,T.Data(:,1),T.Data(:,2),T.Data(:,3),T.Data(:,4),T.Data(:,5),T.Data(:,6),T.Data(:,7),T.Data(:,8),T.Data(:,9),T.Data(:,10),T.Data(:,11),T.Data(:,12),T.Data(:,13),T.Data(:,14),T.Data(:,15),T.Data(:,16),T.Data(:,17),T.Data(:,18),T.Data(:,19),T.Data(:,20),...
     'VariableNames',{'Time','Xpos'     ,'Xspeed'   ,'Xacc'     ,'Ypos'     ,'Yspeed'   ,'Yacc'     ,'Zpos'     ,'Zspeed'   ,'Zacc'     ,'Pitchpos'  ,'Pitchspeed','Pitchacc'  ,'Rollpos'   ,'Rollspeed' ,'Rollacc'   ,'Yawpos'    ,'Yawspeed'  ,'Yawacc'    ,'cP'        ,'cR'});
 
-clear("T","M","out")
+clear("T","out")
+
+SimRegler = SimRegler(1:length(FlightData.Time),:);
+
 
 %% Visualize
 
+
+
+
+
 f = figure;
-f.Position = [312,383,622,455];
+f.Position = [223,200,1251,734];
 f.Name = "Pitch Regelwert";
 hold on
 
-SimCP = SimRegler.PitchCorrection;
-%SimCP = detreend(SimRegler.PitchCorrection);
-%SimCP = SimCP*2;
-%SimCP = SimCP -mean(SimCP) + mean(FlightData.cP);
-%SimCP = movmean(SimCP,100);
 
 
-plot(SimRegler.Time,SimCP)
-plot(FlightData.Time,FlightData.cP)
 
-legend('Simulated', 'Flight')
+plot(M.Time,M.cP,'DisplayName','Flight')
+plot(M.Time,-M.yGyro+0.1,'DisplayName','Calculated')
+
+plot(M.Time,M.cP-M.yGyro,'DisplayName','diff')
+maxDiff=max(M.cP+M.yGyro)
+
+legend()
 
 %% Integrated
 I = figure;
 I.Position = [1097,359,592,480];
-I.Name = "Pitch Regelwert";
+I.Name = "Korrections";
 hold on
-
-plot(FlightData.Time,FlightData.Pitchpos)
-plot(FlightData.Time,FlightData.Pitchspeed)
-%plot(FlightData.Time,cumtrapz(FlightData.Pitchspeed))
-legend('pos', 'speed')
+plot(M.Time,M.cP,'DisplayName','Pitch')
+plot(M.Time,M.cR,'DisplayName','Roll')
+plot(M.Time,M.cYI,'DisplayName','Yaw')
+legend()
 
--- SensorAnalyse/SensorEvaluation.m
+++ SensorAnalyse/SensorEvaluation.m
@@ -1,14 +1,51 @@
 
 
 clc
-close 
+close all 
 
 
 
-D = readtable("../Data/Pendel/YawInertiaPendel02.csv");
+D = readtable("../Data/SensorTest_18.11/FlightLog01.csv");
 D.Time=D.Time-D.Time(1);
-plot(D.Time,D.sensYaw)
+
+
+%% Pich Roll Yaw
+f0 = figure;
+f0.Position = [2,84,1769,893];
+f0.Name = "Pitch"; subplot(311) 
 hold on
-plot(D.Time,D.ZGyro)
-legend('pos','speed')
-%plot(cumtrapz(detrend(D.xGyro)))
+legend()                                                        
+                                                      
+plot(D.Time,D.sensPitch,'DisplayName','sensPitch')
+plot(D.Time,D.yGyro,'DisplayName','yGyro')
+plot(D.Time,cumtrapz(D.yGyro)./100,'DisplayName','yGyro Integrated')
+
+
+
+subplot(312)
+hold on
+plot(D.Time,-D.sensRoll,'DisplayName','sensRoll')
+plot(D.Time,D.xGyro,'DisplayName','xGyro')
+plot(D.Time,cumtrapz(D.xGyro)./100,'DisplayName','xGyro Integrated')
+
+
+subplot(313)
+hold on
+plot(D.Time,D.sensYaw,'DisplayName','sensYaw')
+plot(D.Time,D.ZGyro,'DisplayName','ZGyro')
+plot(D.Time,cumtrapz(D.ZGyro)./100,'DisplayName','ZGyroIntegrated')
+
+
+
+
+
+% 
+% plot(D.Time,D.latitude,'DisplayName','latitude')
+% plot(D.Time,D.longitude,'DisplayName','longitude')
+% plot(D.Time,D.alt,'DisplayName','DisplayName')
+
+legend()
+
+% plot(D.Time,D.xAcc,'DisplayName','xAcc')
+% plot(D.Time,D.yAcc,'DisplayName','yAcc')
+% plot(D.Time,D.zAcc,'DisplayName','zAcc')
